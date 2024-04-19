2a5eebfec2acd7f765da064c5c26b32baf1e33d3
cherrye0ng <dlqndl5297@gmail.com>
2024-04-19 10:45:29 +0900

    first commit

---
+++ Java_Work/src/ex_240419/Hello.java
@@ -0,0 +1,17 @@
+package ex_240419;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 규칙 1, 메인함수(메서드)에서 시작하고 끝난다
+		// 규칙2, 파일명과 클래스명이 동일해야한다
+		// 글자폰트 크기 단축키 : ctrl + shift + "+" 또는 "-"
+		// 코딩창만보기 : ctrl + m
+		// 실행 디버깅 없이  : ctrl + f11
+		System.out.println("Hello Java World");
+		// 클래스 파일 만들고, 오늘 먹을 점심메뉴 출력해보기 
+		System.out.println("고구미 먹을거얌");
+
+	}
+
+}
